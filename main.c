40ffb46b933e355a6daa73282bd84af53c5369f7
Emma Navarro <elnavarro55@gmail.com>
2020-05-14 16:41:37 +0000

    Fix Betty2

--- main.c
+++ main.c
@@ -7,15 +7,8 @@
  */
 int main(int argc, char **argv)
 {
-	char *buff = NULL;
-	stack_t *top = NULL;
-
 	if (argc != 2)
 		arguments_error();
 	execute_opcode(argv);
-	if (buff != NULL)
-		free(buff);
-	_free_stack(top);
-
 	return (0);
 }
--- monty.c
+++ monty.c
@@ -8,10 +8,10 @@ void execute_opcode(char **argv)
 {
 	FILE *fp;
 	char str[1000], *buff = NULL, *token = NULL;
-	unsigned int line_number = 1;
+	unsigned int ln = 1;
 	ssize_t line_size;
 	size_t len = 0;
-	void (*funct)(stack_t **stack, unsigned int line_number);
+	void (*funct)(stack_t **stack, unsigned int ln);
 	stack_t *top = NULL;
 
 	fp = fopen(argv[1], "r");
@@ -22,28 +22,28 @@ void execute_opcode(char **argv)
 		token = strtok(buff, " \t\n\r");
 		if (token == NULL)
 		{
-			line_number += 1;
+			ln += 1;
 			continue;
 		}
 		strcpy(str, token);
-		if (is_hash(token, line_number) == 1)
+		if (is_hash(token, ln) == 1)
 		{
-			line_number += 1;
+			ln += 1;
 			continue;
 		}
 		if (strcmp(token, "push") == 0)
 		{
 			token = strtok(NULL, " \t\n\r");
 			if (token == NULL || is_number(token) == -1)
-				not_int_error(line_number);
-			number = atoi(token);
-			funct = get_op_code(str, line_number), funct(&top, line_number);
+				not_int_error(ln);
+			number = atoi(token), funct = get_op_code(str, ln), funct(&top, ln);
 		}
 		else
-		{
-			funct = get_op_code(token, line_number), funct(&top, line_number);
-		}
-		line_number++;
+			funct = get_op_code(token, ln), funct(&top, ln);
+		ln++;
 	}
 	fclose(fp);
+	if (buff != NULL)
+		free(buff);
+	_free_stack(top);
 }
